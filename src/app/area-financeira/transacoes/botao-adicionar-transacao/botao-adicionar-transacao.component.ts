321e54256be883782762bcb1fc13890b3360a016
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-03-05 16:37:43 -0300

    Mão na massa: botões de radio dinamizados com KeyValuePipe

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 import { FormsModule } from "@angular/forms";
+import { TipoTransacao } from '../../compartilhados/transacao.model';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent, FormsModule],
+  imports: [BotaoComponent, ModalComponent, FormsModule, KeyValuePipe],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
@@ -19,6 +21,8 @@ export class BotaoAdicionarTransacaoComponent {
     data: '',
     conta: ''
   }
+
+  tiposTransacao = TipoTransacao;
 
   abrirModal() {
     this.modalAberto.set(true);
