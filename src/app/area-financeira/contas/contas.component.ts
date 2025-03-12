b3a6e5dcbd0a4859ecfa4779d72c4a5992128a3e
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-03-12 16:08:22 -0300

    Vídeo 5.4 - Criando componente BotaoAdicionarConta

--- src/app/area-financeira/contas/contas.component.ts
+++ src/app/area-financeira/contas/contas.component.ts
@@ -1,12 +1,12 @@
 import { Component, input } from '@angular/core';
-import { BotaoComponent } from "../../compartilhados/botao/botao.component";
 import { ContaComponent } from "./conta/conta.component";
 import { Conta } from '../compartilhados/conta.model';
 import { CardComponent } from "../compartilhados/card/card.component";
+import { BotaoAdicionarContaComponent } from "./botao-adicionar-conta/botao-adicionar-conta.component";
 
 @Component({
   selector: 'app-contas',
-  imports: [BotaoComponent, ContaComponent, CardComponent],
+  imports: [ContaComponent, CardComponent, BotaoAdicionarContaComponent],
   templateUrl: './contas.component.html',
   styleUrl: './contas.component.css'
 })
